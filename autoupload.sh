c9ad37921f97549e697a0b30a95fdf662d343307
Oliver Hilsky <oliver@hilsky.de>
2021-12-04 00:56:41 +0100

    added retries to upload

--- autoupload.sh
+++ autoupload.sh
@@ -10,6 +10,7 @@ REMOTE_NAME="nextcloud:"
 inotifywait -m $DOCUMENT_PATH -e create -e moved_to |
     while read dir action file; do
         echo "The file '$file' appeared in directory '$dir' via '$action'"
-        rclone --config /opt/autouploader/rclone.conf move ${dir}/${file} ${REMOTE_NAME}
+        # retry is used as the upload might take some time after the initial creation of the file. Remove if not needed
+        rclone --config /opt/autouploader/rclone.conf --retries-sleep 1s --retries 10 move ${dir}/${file} ${REMOTE_NAME}
         echo "Moved file '$file' to remote '$REMOTE_NAME'"
     done
